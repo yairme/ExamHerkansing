bf9c4a20d242c1a5d66728fefccf625e08144aad
Stefan Hol <70897837+stefan-hol@users.noreply.github.com>
2024-06-09 14:51:21 +0200

    Raycast error fixed
    fixed a problem with the raycast turning the wrong way and being to long. (Movement.cs)

--- NightTaxi/Assets/Scripts/Movement.cs
+++ NightTaxi/Assets/Scripts/Movement.cs
@@ -7,13 +7,16 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] private float BaseSpeed;
     [SerializeField] private float WallDistance;
+    [SerializeField] private Direction CurentDirection = Direction.UP;
     private float PlayerSpeed;
-    private Direction CurentDirection = Direction.UP;
+    private Vector3 ForwardDir;
     private Direction Pref;
 
     private void Start()
     {
         PlayerSpeed = BaseSpeed;
+        Pref = CurentDirection;
+        SetDirection();
     }
 
     void FixedUpdate()
@@ -65,39 +68,65 @@ public class Movement : MonoBehaviour
             }
         }
     }
+
     private void MakeTurn(Direction _Direction, int _Degree)
     {
         transform.rotation = Quaternion.Euler(0.0f, _Degree, 0.0f);
         CurentDirection = _Direction;
         PlayerSpeed = BaseSpeed;
+        SetDirection();
     }
+
+    private void SetDirection()
+    {
+        switch (CurentDirection)
+        {
+            case Direction.UP:
+                ForwardDir = new Vector3(0, 0, 1);
+                break;
+            case Direction.DOWN:
+                ForwardDir = new Vector3(0, 0, -1);
+                break;
+            case Direction.LEFT:
+                ForwardDir = new Vector3(-1, 0, 0);
+                break;
+            case Direction.RIGHT:
+                ForwardDir = new Vector3(1, 0, 0);
+                break;
+        }
+    }
+
     private bool CheckTurn()
     {
+        if(Pref == CurentDirection)
+        {
+            return false;
+        }
         switch (Pref)
         {
             case Direction.UP:
-                if (!Physics.Raycast(transform.position, new Vector3(0, 0, 1), WallDistance))
+                if (!Physics.Raycast(transform.position, new Vector3(0, 0, 1), WallDistance, 6))
                 {
                     MakeTurn(Pref, 0);
                     return true;
                 }
                 break;
             case Direction.RIGHT:
-                if (!Physics.Raycast(transform.position, new Vector3(-1, 0, 0), WallDistance))
+                if (!Physics.Raycast(transform.position, new Vector3(1, 0, 0), WallDistance, 6))
                 {
                     MakeTurn(Pref, 90);
                     return true;
                 }
                 break;
             case Direction.DOWN:
-                if (!Physics.Raycast(transform.position, new Vector3(0, 0, -1), WallDistance))
+                if (!Physics.Raycast(transform.position, new Vector3(0, 0, -1), WallDistance, 6))
                 {
                     MakeTurn(Pref, 180);
                     return true;
                 }
                 break;
             case Direction.LEFT:
-                if (!Physics.Raycast(transform.position, new Vector3(1, 0, 0), WallDistance))
+                if (!Physics.Raycast(transform.position, new Vector3(-1, 0, 0), WallDistance, 6))
                 {
                     MakeTurn(Pref, 270);
                     return true;
@@ -111,7 +140,11 @@ public class Movement : MonoBehaviour
         if (other.CompareTag("Corner"))
         {
             transform.position = other.transform.position;
-            if (!CheckTurn() && Physics.Raycast(transform.position, transform.forward, WallDistance))
+            if (CheckTurn())
+            {   
+                return;
+            }
+            if (Physics.Raycast(transform.position, ForwardDir, WallDistance))
             {
                 PlayerSpeed = 0;
             }
